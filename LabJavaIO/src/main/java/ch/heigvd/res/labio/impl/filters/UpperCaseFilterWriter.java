11bc4950783fecee9bf324ce7b0cbeab950295e7
Insuline13 <alexandra.cerottini@heig-vd.ch>
2021-03-05 16:50:57 +0100

    UpperCaseFilterWriter: remove comments

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -16,13 +16,11 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
     super.write(str.toUpperCase(),off,len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
     for(int i = 0; i < cbuf.length; ++i){
       cbuf[i] = Character.toUpperCase(cbuf[i]);
     }
