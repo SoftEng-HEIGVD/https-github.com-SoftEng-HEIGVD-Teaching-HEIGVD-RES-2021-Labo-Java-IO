2da72816584c8c51d9076a8cdb797061d22d89ff
Quillasp <saralfddin93@yahoo.fr>
2021-03-05 13:05:29 +0100

    DFSFileExplorer done

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -2,6 +2,9 @@ package ch.heigvd.res.labio.impl.explorers;
 
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 import java.io.File;
 
@@ -17,7 +20,25 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (rootDirectory == null)
+      return;
+
+    vistor.visit(rootDirectory);
+
+    File[] childFiles = rootDirectory.listFiles();
+
+    if(childFiles == null)
+      return;
+
+    Arrays.sort(childFiles);
+
+    for (File f : childFiles) {
+      if (f.isDirectory())
+        explore(f, vistor);
+      else
+        vistor.visit(f);
+    }
   }
 
 }
