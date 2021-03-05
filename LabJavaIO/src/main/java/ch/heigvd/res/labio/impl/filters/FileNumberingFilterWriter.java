c625f347b6ab4ca6fecc794bab4462953fd64ef2
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-03-05 17:17:53 +0100

    Implémentation du FileNumberingFilterWriter
    Les tests passent

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,8 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
+import jdk.jshell.execution.Util;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -10,32 +13,78 @@ import java.util.logging.Logger;
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
  * process.
- *
+ * <p>
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
  * @author Olivier Liechti
  */
-public class FileNumberingFilterWriter extends FilterWriter {
+public class FileNumberingFilterWriter extends FilterWriter
+{
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+   private int lineNumber;
+   private char lastChar;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+   public FileNumberingFilterWriter(Writer out)
+   {
+      super(out);
+      lineNumber = 1;
+   }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+   private void beginNewLine() throws IOException
+   {
+      String str = lineNumber + "\t";
+      super.write(str, 0, str.length());
+      ++lineNumber;
+   }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+   @Override
+   public void write(String str, int off, int len) throws IOException
+   {
+//      if(str == null || str.isEmpty()) return;
+//      String[] result;
+//      String sub = str.substring(off, off+len);
+//      result = Utils.getNextLine(sub);
+//
+//      while(!result[0].isEmpty())
+//      {
+//         beginNewLine();
+//         super.write(result[0]);
+//
+//         result = Utils.getNextLine(result[1]);
+//      }
+//
+//      beginNewLine();
+//      super.write(result[1]);
+      write(str.toCharArray(), off, len);
+   }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+   @Override
+   public void write(char[] cbuf, int off, int len) throws IOException
+   {
+      for (int i = off; i < off + len; ++i)
+      {
+         write(cbuf[i]);
+      }
+   }
+
+   @Override
+   public void write(int c) throws IOException
+   {
+      if(lineNumber == 1 || lastChar == '\r' && c != '\n')
+      {
+         beginNewLine();
+      }
+
+      super.write(c);
+
+      if(c == '\n')
+      {
+         beginNewLine();
+      }
+
+      lastChar = (char)c;
+
+   }
 
 }
