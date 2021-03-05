89f8afbe9b7f86f17842923d0e0d89e1084d6d6a
Quentin Forestier <quentin.forestier@heig-vd.ch>
2021-03-05 12:06:50 +0100

    Implémentation de utils
    Tous les tests passent

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -3,24 +3,44 @@ package ch.heigvd.res.labio.impl;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Olivier Liechti
  */
-public class Utils {
+public class Utils
+{
 
-  private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+    private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
-  /**
-   * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
-   * @param lines a string that may contain 0, 1 or more lines
-   * @return an array with 2 elements; the first element is the next line with
-   * the line separator, the second element is the remaining text. If the argument does not
-   * contain any line separator, then the first element is an empty string.
-   */
-  public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    /**
+     * This method looks for the next new line separators (\r, \n, \r\n) to extract
+     * the next line in the string passed in arguments.
+     *
+     * @param lines a string that may contain 0, 1 or more lines
+     * @return an array with 2 elements; the first element is the next line with
+     * the line separator, the second element is the remaining text. If the argument does not
+     * contain any line separator, then the first element is an empty string.
+     */
+    public static String[] getNextLine(String lines)
+    {
+
+        String[] result = new String[]{"", ""};
+        String[] newLineSeparator = new String[]{"\r\n", "\r", "\n"};
+
+        int index = -1;
+
+        for (String ls : newLineSeparator)
+        {
+            if ((index = lines.indexOf(ls)) != -1)
+            {
+                index += ls.length()-1;
+                break;
+            }
+        }
+
+
+        result[0] = lines.substring(0, index + 1);
+        result[1] = lines.substring(index + 1);
+
+        return result;
+    }
 
 }
