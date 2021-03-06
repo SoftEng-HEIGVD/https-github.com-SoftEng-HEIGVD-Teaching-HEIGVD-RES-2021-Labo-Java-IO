2d1db490e771c5e758bb9ffe771c4cffa527223e
K-do <miguel.dovalelopes@heig-vd.ch>
2021-03-06 14:03:04 +0100

    FileTransformer upgraded with buffers

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
@@ -52,6 +52,11 @@ public abstract class FileTransformer implements IFileVisitor {
        * writer has been decorated by the concrete subclass!). You need to write a loop to read the
        * characters and write them to the writer.
        */
+
+      /* Decorate I/O streams with buffer */
+      reader = new BufferedReader(reader);
+      writer = new BufferedWriter(writer);
+
       int data = reader.read();
       while(data != -1){
         writer.write((char) data);
