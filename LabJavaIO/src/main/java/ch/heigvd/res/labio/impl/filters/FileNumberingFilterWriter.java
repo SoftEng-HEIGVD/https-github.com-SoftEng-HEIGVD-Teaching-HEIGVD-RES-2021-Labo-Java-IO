ea50257bc46566b281af534c3fcc465c84dbd158
Alessandro <alessandro.parrinoap@gmail.com>
2021-03-07 18:33:55 +0100

    implements FileNumberingFilterWriter methods

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -12,49 +14,59 @@ import java.util.logging.Logger;
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
  * process.
- *
+ * <p>
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    int lineNumber = 1;
+    int pastChar;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
-
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-  /*  List<Integer> indexes = new ArrayList<>();
-    int index = 0;
-
-
-    while(index != -1){
-      index = str.indexOf("\n", index);
-      if (index != -1) {
-        indexes.add(index + 1);
-        index++;
-      }
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
     }
 
-    StringBuilder builder = new StringBuilder("1\t"+ str);
-    int size = indexes.size() + 1;
-    for(int i = 2; i < size; ++i){
-      builder.insert(indexes.get(i - 1), i + "\t");
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        str = str.substring(off,off+len);
+        StringBuilder builder = new StringBuilder("");
+        boolean oneLine = Utils.getNextLine(str)[0].equals("");
+        if (oneLine){ if(lineNumber == 1) builder.append(lineNumber++ + "\t" + Utils.getNextLine(str)[1]);
+                      else builder.append(Utils.getNextLine(str)[1]);}
+        else {
+            if (lineNumber == 1) builder.append(lineNumber++ + "\t" + Utils.getNextLine(str)[0] + lineNumber++ + "\t");
+            else builder.append(Utils.getNextLine(str)[0] + lineNumber++ + "\t");
+
+            String tmp = Utils.getNextLine(str)[1];
+
+            while (!Utils.getNextLine(tmp)[0].equals("")) {
+                builder.append(Utils.getNextLine(tmp)[0] + lineNumber++ + "\t");
+                tmp = Utils.getNextLine(tmp)[1];
+            }
+            builder.append(Utils.getNextLine(tmp)[1]);
+        }
+        super.write(builder.toString(), 0, builder.toString().length());
     }
-    super.write(builder.toString(), off, len);*/
-  }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        write(new String(cbuf), off, len);
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(int c) throws IOException {
+        if(lineNumber == 1) super.write(lineNumber++ + "\t");
+        if(c != '\n' && pastChar == '\r') super.write(lineNumber++ + "\t");
+        if(c == '\n'){
+            super.write(c);
+            super.write(lineNumber++ + "\t");
+        }
+        else
+        super.write(c);
+        pastChar = c;
+    }
 
 }
