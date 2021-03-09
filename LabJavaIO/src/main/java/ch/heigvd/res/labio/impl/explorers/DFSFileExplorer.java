3e40b82aec65fe2b4782f0303a96b83c65423a8c
Ilias <igoujgali@gmail.com>
2021-03-09 22:35:03 +0100

    Dérécursification du DFS

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -5,6 +5,7 @@ import ch.heigvd.res.labio.interfaces.IFileVisitor;
 
 import java.io.File;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
@@ -14,40 +15,36 @@ import java.util.Stack;
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor visitor) {
-      if(rootDirectory == null){
-          return;
-      }
+    @Override
+    public void explore(File rootDirectory, IFileVisitor visitor) {
+        if (rootDirectory == null) {
+            return;
+        }
+        List<File> list = new ArrayList<File>();
+        list.add(rootDirectory);
 
-      visitor.visit(rootDirectory);
+        while (!list.isEmpty()) {
+            File currentFile = list.get(0);
+            list.remove(0);
 
-      File[] files = rootDirectory.listFiles();
+            visitor.visit(currentFile);
 
-      if (files == null){
-          return;
-      }
+            File[] files = currentFile.listFiles();
 
-      Arrays.sort(files);
+            if (files == null) {
+                return;
+            }
 
-      for(File file : files){
-          if(file.isDirectory()){
-              explore(file, visitor);
-          }else{
-              visitor.visit(file);
-          }
+            Arrays.sort(files);
 
-      }
-
-
-
-
-
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+            if (currentFile.isDirectory()) {
+                list.addAll(0,Arrays.asList(files));
+            }
+        }
+    }
 }
