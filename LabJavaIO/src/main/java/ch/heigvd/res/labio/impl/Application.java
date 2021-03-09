322f206eb1c30d890e5c3b9b94b9f0f68defe8a1
Dosseggegw1 <gwen.dos@bluewin.ch>
2021-03-09 18:49:38 +0100

    Application en cours

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -7,10 +7,9 @@ import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import ch.heigvd.res.labio.quotes.QuoteClient;
 import ch.heigvd.res.labio.quotes.Quote;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+
+import java.io.*;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
@@ -90,6 +89,7 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
+      storeQuote(quote,"quote-" + i + ".utf8");
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
@@ -109,7 +109,7 @@ public class Application implements IApplication {
 
   /**
    * This method stores the content of a quote in the local file system. It has
-   * 2 responsibilities: 
+   * 2 responsibilities:
    * 
    * - with quote.getTags(), it gets a list of tags and uses
    *   it to create sub-folders (for instance, if a quote has three tags "A", "B" and
@@ -123,7 +123,19 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    List<String> tags = quote.getTags();
+    String pathDir = WORKSPACE_DIRECTORY;
+    for(String tag : tags){
+      pathDir += (tag + '/');
+    }
+
+    File directory = new File(pathDir);
+    directory.mkdirs();
+    String pathFile = pathDir + filename;
+
+    FileWriter fileWriter = new FileWriter(pathFile);
+    fileWriter.write(quote.getQuote());
+    fileWriter.close();
   }
   
   /**
@@ -140,6 +152,12 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+        try{
+          writer.write(file.getPath());
+        }
+        catch(IOException e){
+          e.printStackTrace();
+        }
       }
     });
   }
