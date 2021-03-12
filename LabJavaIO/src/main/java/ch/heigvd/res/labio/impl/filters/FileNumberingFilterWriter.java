1cc9bca6edf07738fb9bf90e947210b60f2f296f
Hoang Anh Mai <hoanganh.mai@hes-so.ch>
2021-03-12 19:18:42 +0100

    FileNumberingFilterWriter finished but REALLY need to optimize

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -17,6 +17,10 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
+  private int lines = 0;
+  private boolean sepL = false;
+  private boolean sepW = false;
+
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
   public FileNumberingFilterWriter(Writer out) {
@@ -25,17 +29,65 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    StringBuilder s1 = new StringBuilder();
+    Integer lineNumber;
+    if(lines == 0) {
+      lineNumber = ++lines;
+      s1.append(lineNumber);
+      s1.append('\t');
+    }
+
+    for(int i = off; i < len + off; i++) {
+      if(lines == 0) {
+        lineNumber = ++lines;
+        s1.append(lineNumber);
+        s1.append('\t');
+      }
+
+      if(sepW ) {
+        lineNumber = ++lines;
+        s1.append(lineNumber);
+        s1.append('\t');
+        s1.append(str.charAt(i));
+        sepW = false;
+        sepL = false;
+      } else if(str.charAt(i) != '\n' && sepL) {
+        lineNumber = ++lines;
+        s1.append(lineNumber);
+        s1.append('\t');
+        s1.append(str.charAt(i));
+        sepL = false;
+      } else if(str.charAt(i) == '\n') {
+        s1.append(str.charAt(i));
+        sepW = true;
+      } else if(str.charAt(i) == '\r' ) {
+        s1.append(str.charAt(i));
+        sepL = true;
+      } else {
+        s1.append(str.charAt(i));
+      }
+    }
+
+    if(len != 1 && (sepL || sepW)) {
+      lineNumber = ++lines;
+      s1.append(lineNumber);
+      s1.append('\t');
+      sepW = false;
+    }
+
+    super.write(s1.toString(), 0, s1.length());
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    this.write(cbuf.toString(), off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    this.write(Character.toString((char) c), 0, 1);
   }
 
 }
