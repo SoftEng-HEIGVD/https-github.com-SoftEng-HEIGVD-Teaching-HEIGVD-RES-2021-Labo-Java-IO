5ddb8c2449557abafcf813c9ea18baa89adea9cd
JunodChristophe <49147076+JunodChristophe@users.noreply.github.com>
2021-03-12 10:52:20 +0100

    Implémentation pour passer le test itShouldPrintANumberForFileWithOneLine() De FileNumberingFilterWriterTest. Fonction modifiée :   write(String str, int off, int len) de la classe FileNumberingFilterWriter.   Nouveau champ privé "count" dans la même classe. Afin de compter le nombre de ligne traîtée par le writer A la fin de l'implémentation les tests suivant ont aussi passés :   itSouldPrintANumberForFileWithTwoLines()   itSouldWorkOnWindows()   itSouldHandleWriteWithPartOfAString()   itSouldWorkIfThereIsNoNewLineAtTheEnd()   itSouldWorkWithPartialLines()   itSouldWorkWhenThereAreOneTwoOrThreeDigitsInTheLineNumber()   itSouldWorkOnMacOS9()   itSouldWorkOnUnix()

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -18,6 +20,7 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int count = 1; // To count the number of line processed.
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -25,7 +28,26 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // Process preparation
+    String[] subStr = new String[2];
+    subStr[1] = str.substring(off,off+len);
+    StringBuilder resultStr = new StringBuilder();
+
+    /* There are 2 situations where we implement the line number.
+     * For the first line and when there is a line separator. */
+    if (count == 1) { // For the first line.
+      resultStr.append(count).append("\t");
+      count++;
+    }
+    subStr = Utils.getNextLine(subStr[1]);
+    while(!subStr[0].isEmpty()) { // Check if there is a line separator.
+      resultStr.append(subStr[0]).append(count).append("\t");
+      count++;
+      subStr = Utils.getNextLine(subStr[1]); // Check for another line separator.
+    }
+    resultStr.append(subStr[1]); // The remaining
+
+    this.out.write(resultStr.toString());
   }
 
   @Override
