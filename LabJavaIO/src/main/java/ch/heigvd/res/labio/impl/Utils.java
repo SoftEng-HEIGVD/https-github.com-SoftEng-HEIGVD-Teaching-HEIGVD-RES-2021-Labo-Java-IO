a7a030474bd9adabdf342c38cd8b407b6aee9156
AliceThunderWind <dalia.maillefer@gmail.com>
2021-03-12 12:20:32 +0100

    Tests in Utils are working

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -20,7 +20,7 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String line[] = lines.split("(?<=\n|\r(?!\n))", 2);
+    return line.length == 2 ? line : new String[]{"",line[0]};
   }
-
 }
