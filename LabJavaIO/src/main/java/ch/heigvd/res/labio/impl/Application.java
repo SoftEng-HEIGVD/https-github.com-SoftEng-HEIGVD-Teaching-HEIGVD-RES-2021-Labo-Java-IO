b61f43130b4be727b0eadf8354ffbb3d4b327c07
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-03-15 21:18:42 +0100

    Commit with the "Access Refused" error

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -9,11 +9,9 @@ import ch.heigvd.res.labio.quotes.Quote;
 import ch.heigvd.res.labio.quotes.QuoteClient;
 import org.apache.commons.io.FileUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+import java.io.*;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -98,7 +96,7 @@ public class Application implements IApplication {
          * one method provided by this class, which is responsible for storing the content of the
          * quote in a text file (and for generating the directories based on the tags).
          */
-
+        storeQuote(quote, "quote-" + i + ".utf8");
         LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
         for (String tag : quote.getTags()) {
           LOG.info("> " + tag);
@@ -134,7 +132,21 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    List<String> tags = quote.getTags();
+    String quoteText = quote.getQuote();
+
+    String pathToRep = WORKSPACE_DIRECTORY + "/";
+    for(String tag : tags){
+      pathToRep += tag + "/";
+    }
+    File fileDir = new File(pathToRep);
+    fileDir.mkdirs();
+
+    FileWriter fileWriter = new FileWriter(pathToRep);
+    fileWriter.write(quoteText);
+    fileWriter.close();
   }
   
   /**
@@ -151,6 +163,11 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+        try{
+          writer.write(file.getPath());
+        }
+        catch(IOException e){
+        }
       }
     });
   }
