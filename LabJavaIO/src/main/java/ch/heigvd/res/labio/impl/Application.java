1ec734e299863c00f78d9b6929f850ff23068e33
Noah Fusi <noah.fusi@heig-vd.ch>
2021-03-15 13:39:45 +0100

    Working on Application.java Test theApplicationShouldGenerateTheCorrectNumberOfQuotFiles passes Test theApplicationShouldUseTheCorrectSyntaxToNameTheQuoteFiles passes

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -98,6 +99,8 @@ public class Application implements IApplication {
          * one method provided by this class, which is responsible for storing the content of the
          * quote in a text file (and for generating the directories based on the tags).
          */
+
+        storeQuote(quote, "quote-" + i + ".utf8");
         LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
         for (String tag : quote.getTags()) {
           LOG.info("> " + tag);
@@ -133,7 +136,21 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    //Create quotes directory if it doesn't exists
+    StringBuilder filePath = new StringBuilder(WORKSPACE_DIRECTORY);
+    List<String> tags = quote.getTags();
+    for(String s : tags)
+    {
+      filePath.append('/').append(s);
+    }
+
+    filePath.append('/').append(filename);
+    File newQuote = new File(filePath.toString());
+    newQuote.getParentFile().mkdirs();
+    newQuote.createNewFile();
+
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
   }
   
   /**
@@ -145,11 +162,20 @@ public class Application implements IApplication {
     explorer.explore(new File(WORKSPACE_DIRECTORY), new IFileVisitor() {
       @Override
       public void visit(File file) {
+        try
+        {
+          writer.write(file.getPath() + "\n");
+        }
+        catch (IOException ignored)
+        {
+
+        }
         /*
          * There is a missing piece here. Notice how we use an anonymous class here. We provide the implementation
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+
       }
     });
   }
