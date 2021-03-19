2cbf0b454ad24809e025a83add97705e38c0c231
FionaGbn <fiona.gamboni@heig-vd.ch>
2021-03-19 13:06:12 +0100

    FileNumberFilterWriter complété, tests fonctionnent

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -18,24 +18,48 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private static int cnt;
+  private boolean backslashR = false;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
+    cnt = 0;
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = off; i < off + len; ++i) {
+      write(str.charAt(i));
+    }
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = off; i < off + len; ++i) {
+      write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
 
+    if (cnt == 0) { // First line
+      out.write(++cnt +  "\t");
+      super.write(c);
+      return;
+    } else if (c == '\n') { // Unix and Windows case
+      super.write(c);
+      out.write(++cnt +  "\t");
+      backslashR = false;
+      return;
+    } else if (backslashR) { // Mac case
+      out.write(++cnt +  "\t");
+      backslashR = false;
+    } else if (c == '\r') { // Potential Mac or Windows case : we must wait the next char to find out
+      backslashR = true;
+    }
+    super.write(c);
+
+  }
 }
+
