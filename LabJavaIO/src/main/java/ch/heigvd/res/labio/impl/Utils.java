affd9cfc3a19e1f95ed75f2be2bfbe0da661c31a
Mario Tomic <mario-tomic@hotmail.com>
2021-03-19 17:08:31 +0100

    On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
    
     Changes not staged for commit:
    	modified:   LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/CompleteFileTransformer.java
    	modified:   LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
    	modified:   LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/NoOpFileTransformer.java

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -12,15 +12,29 @@ public class Utils {
 
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
+   * the next line in the string passed in arguments.
+   *
    * @param lines a string that may contain 0, 1 or more lines
    * @return an array with 2 elements; the first element is the next line with
    * the line separator, the second element is the remaining text. If the argument does not
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    int firstOccurenceOfLineBreak = 0;
 
+    String[] lineBreakers = {"\n", "\r", "\r\n"};
+    for (String s : lineBreakers) {
+      firstOccurenceOfLineBreak = lines.indexOf(s);
+      if (firstOccurenceOfLineBreak != -1) {
+        break;
+      }
+    }
+
+    if (firstOccurenceOfLineBreak == -1) {
+      return new String[]{"", lines};
+    }
+
+    return new String[]{lines.substring(0, firstOccurenceOfLineBreak + 1), lines.substring(firstOccurenceOfLineBreak + 1)};
+
+  }
 }
