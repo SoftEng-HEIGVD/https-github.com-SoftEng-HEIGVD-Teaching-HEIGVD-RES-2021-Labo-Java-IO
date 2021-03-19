30a2f9d6ea6d26ccdd1e45557c0dec569e50ae24
yanik23 <ylange@windowslive.com>
2021-03-19 12:17:54 +0100

    some FileNumberingFilterWriterTests working

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -24,15 +24,17 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
 
   public FileNumberingFilterWriter(Writer out) {
-    super(out);
+      super(out);
+      lineCounter = 1;
   }
 
+  static long lineCounter = 1;
   @Override
   public void write(String str, int off, int len) throws IOException {
     //throw new UnsupportedOperationException("The student has not implemented this method yet.");
       String tempStr = "";
-      long lineCounter = 1;
 
+      //long lineCounter = 1;
       String [] oneLineAndRest = Utils.getNextLine(str);
 
       do {
@@ -41,12 +43,28 @@ public class FileNumberingFilterWriter extends FilterWriter {
           tempStr += oneLineAndRest[0];
           ++lineCounter;
           oneLineAndRest = Utils.getNextLine(oneLineAndRest[1]);
-      }while(oneLineAndRest[0] != "");
+      }while(!oneLineAndRest[0].equals(""));
 
-      tempStr += lineCounter;
-      tempStr += "\t";
-      tempStr += oneLineAndRest[1];
+      /*while(oneLineAndRest[0] != "") {
+          tempStr += lineCounter;
+          tempStr += "\t";
+          tempStr += oneLineAndRest[0];
+          ++lineCounter;
+          oneLineAndRest = Utils.getNextLine(oneLineAndRest[1]);
+      }*/
 
+      //if(oneLineAndRest[0] != ""){
+      //if(oneLineAndRest[oneLineAndRest.length-1] != "\n"){
+
+      if(!oneLineAndRest[1].equals((""))) {
+          tempStr += lineCounter;
+          tempStr += "\t";
+          tempStr += oneLineAndRest[1];
+      } else {
+          tempStr += lineCounter;
+          tempStr += "\t";
+      }
+      
       out.write(tempStr);
 
   }
@@ -58,10 +76,10 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(int c) throws IOException {
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
-      char result = (char)c;
-      String str =String.valueOf(result);
-      
+    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      //char result = (char)c;
+      //String str =String.valueOf(result);
+
   }
 
 }
