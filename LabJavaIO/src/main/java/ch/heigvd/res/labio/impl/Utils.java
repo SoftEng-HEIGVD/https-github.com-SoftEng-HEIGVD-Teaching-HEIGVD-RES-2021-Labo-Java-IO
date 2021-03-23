3f46c1353747908f11787cd372d0439ea8a410ec
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-03-23 17:46:45 +0100

    Utils tests are passed now, had multiple mistake due to a dumb copy paste from me + I was doing windows last which couldn't work since it contained 2 char I had already checked, put it first now and everything works fine

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -24,24 +24,24 @@ public class Utils {
   public static String[] getNextLine(String lines) {
 
     //First we want to know the index of the line separators
-    int indexR = lines.indexOf("\r");
-    int indexN = lines.indexOf("\n");
-    int indexRN = lines.indexOf("\r\n");
+    int indexR = lines.indexOf("\r");//MAC OS
+    int indexN = lines.indexOf("\n");//UNIX
+    int indexRN = lines.indexOf("\r\n");//WINDOWS
 
     //Then we want to check if those indexes exist or not,
     // if the index = -1 then the line separator is not in the "lines" string
     // Once we know if they exist, we want to start forming our return element String[]
     // and actually return it if the line separator exists
     // we return something too if we don't find any line separators
+    //here we add 2 to the index because the line separator is 2 characters
+    if(indexRN != -1)
+      return new String[]{lines.substring(0, indexRN + 2), lines.substring(indexRN + 2)};
+
     if(indexR != -1)
       return new String[]{lines.substring(0, indexR + 1), lines.substring(indexR + 1)};
 
     if(indexN != -1)
-      return new String[]{lines.substring(0, indexR + 1), lines.substring(indexR + 1)};
-
-    //here we add 2 to the index because the line separator is 2 characters
-    if(indexRN != -1)
-      return new String[]{lines.substring(0, indexR + 2), lines.substring(indexR + 2)};
+      return new String[]{lines.substring(0, indexN + 1), lines.substring(indexN + 1)};
 
     return new String[]{"", lines};
   }
