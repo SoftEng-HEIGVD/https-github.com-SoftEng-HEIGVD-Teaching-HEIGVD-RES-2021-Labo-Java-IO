a112a0f7dc032e4951a0281dbc1c417f3860597a
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-03-23 15:07:57 +0100

    DFSFileExplorer method

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -4,6 +4,7 @@ import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -12,12 +13,32 @@ import java.io.File;
  * files in the directory and then moves into the subdirectories.
  * 
  * @author Olivier Liechti
+ *
+ * Modified by Blanc Jean-Luc
  */
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  public void explore(File rootDirectory, IFileVisitor visitor) {
+
+    //Checking that rootDirectory does exist before going further
+    if(rootDirectory == null){
+      return;
+    }
+
+    //if it exists, then we can visit it
+    visitor.visit(rootDirectory);
+
+    //if the file is actually a directory, then we need to go further in
+    if(rootDirectory.isDirectory()){
+      //we create an array that contains the directory content, we then sort this content in order to display it
+      File[] folderContent =  rootDirectory.listFiles();
+      Arrays.sort(folderContent);
+      //adding recursive here
+      for(File file : folderContent){
+        explore(file, visitor);
+      }
+    }
   }
 
 }
