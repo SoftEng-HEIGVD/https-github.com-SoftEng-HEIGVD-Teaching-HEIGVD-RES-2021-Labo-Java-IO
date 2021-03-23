834112452d13acdb5468bf700a12d82e6b9a44b4
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-03-23 17:10:58 +0100

    UpperCaseFilterWriter Method

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -3,11 +3,16 @@ package ch.heigvd.res.labio.impl.filters;
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Locale;
 
 /**
  *
  * @author Olivier Liechti
+ *
+ * Modified by Blanc Jean-Luc
  */
+
+//basically here we only have to implement last class methods using "super" while making sure we put everything to upper case
 public class UpperCaseFilterWriter extends FilterWriter {
   
   public UpperCaseFilterWriter(Writer wrappedWriter) {
@@ -16,17 +21,20 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    super.write(str.toUpperCase(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = 0; i < cbuf.length; i++){
+      cbuf[i] = Character.toUpperCase(cbuf[i]);
+    }
+    super.write(cbuf, off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    super.write(Character.toUpperCase(c));
   }
 
 }
