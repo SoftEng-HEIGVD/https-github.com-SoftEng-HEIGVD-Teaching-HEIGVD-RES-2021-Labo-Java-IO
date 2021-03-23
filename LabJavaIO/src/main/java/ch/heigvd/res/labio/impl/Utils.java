622be75dbc9f5efe40c5e0a1cebdf4c422d914a0
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-03-23 14:34:09 +0100

    Utils method

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -5,6 +5,8 @@ import java.util.logging.Logger;
 /**
  *
  * @author Olivier Liechti
+ *
+ * Modified by Blanc Jean-Luc
  */
 public class Utils {
 
@@ -20,7 +22,28 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    //First we want to know the index of the line separators
+    int indexR = lines.indexOf("\r");
+    int indexN = lines.indexOf("\n");
+    int indexRN = lines.indexOf("\r\n");
+
+    //Then we want to check if those indexes exist or not,
+    // if the index = -1 then the line separator is not in the "lines" string
+    // Once we know if they exist, we want to start forming our return element String[]
+    // and actually return it if the line separator exists
+    // we return something too if we don't find any line separators
+    if(indexR != -1)
+      return new String[]{lines.substring(0, indexR + 1), lines.substring(indexR + 1)};
+
+    if(indexN != -1)
+      return new String[]{lines.substring(0, indexR + 1), lines.substring(indexR + 1)};
+
+    //here we add 2 to the index because the line separator is 2 characters
+    if(indexRN != -1)
+      return new String[]{lines.substring(0, indexR + 2), lines.substring(indexR + 2)};
+
+    return new String[]{"", lines};
   }
 
 }
